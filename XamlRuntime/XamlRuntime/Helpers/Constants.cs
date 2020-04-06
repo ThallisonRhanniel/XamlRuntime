b373a815f9428fb406c72f6c6a7df9a0b9c6d762
Thallison Rhanniel Garcia Lima <thallisonrlima@gmail.com>
2020-04-06 11:50:24 -0300

    Add project files.

---
+++ XamlRuntime/XamlRuntime/Helpers/Constants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamlRuntime.Helpers
+{
+    public static class RuntimePage
+    {
+        public const string Header =
+            @"<?xml version='1.0' encoding='utf-8' ?>
+             <ContentPage xmlns='http://xamarin.com/schemas/2014/forms'
+             xmlns:x='http://schemas.microsoft.com/winfx/2009/xaml'
+             xmlns:d='http://xamarin.com/schemas/2014/forms/design'
+             xmlns:mc='http://schemas.openxmlformats.org/markup-compatibility/2006'
+             Title='Thallison'    
+             mc:Ignorable='d'  >
+
+            <ScrollView>
+                <StackLayout x:Name='StackLayout1' HorizontalOptions='FillAndExpand' VerticalOptions='FillAndExpand' >";
+
+
+        public const string Label = 
+            @"<Label Text='Welcome to Xamarin.Forms!'
+             x:Name='lblWelcome' VerticalOptions='CenterAndExpand' 
+             HorizontalOptions='CenterAndExpand' />";
+
+
+        public static string StartLabel = 
+            @"<Label Text='Welcome to Xamarin.Forms!'
+               x:Name='";
+
+        public static string EndLabel = 
+            @"' VerticalOptions='CenterAndExpand' 
+              HorizontalOptions='CenterAndExpand' />";
+
+
+
+        public const string Footer = 
+            @"</StackLayout>
+                </ScrollView>
+                    </ContentPage>";
+    }
+}
---
+++ XamlRuntime/XamlRuntime/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Prism.Navigation.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamlRuntime.Helpers;
+
+namespace XamlRuntime.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        public DelegateCommand OpenNewSimplePageCommand { get; private set; }
+
+        public DelegateCommand OpenNewPageCommand { get; private set; }
+
+        public DelegateCommand OpenNewPage2Command { get; private set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            OpenNewSimplePageCommand = new DelegateCommand(async () =>
+            {
+                // See the sample for the full XAML string
+                string pageXAML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<ContentPage xmlns=\"http://xamarin.com/schemas/2014/forms\"\nxmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\nx:Class=\"LoadRuntimeXAML.CatalogItemsPage\"\nTitle=\"Catalog Items\">\n<StackLayout>\n<Label x:Name=\"monkeyName\"\n Text=\"Nome: Thallison\"\n Margin=\"20\"\n  Font=\"Large\"\n  />\n</StackLayout>\n</ContentPage>";
+
+                ContentPage page = new ContentPage().LoadFromXaml(pageXAML);
+                await Prism.PrismApplicationBase.Current.MainPage.Navigation.PushAsync(page);
+            });
+
+            OpenNewPageCommand = new DelegateCommand(async () =>
+            {
+                string xaml = $"{RuntimePage.Header}" +
+                              $"{RuntimePage.Label}" +
+                              $"{RuntimePage.Footer}";
+                
+
+                ContentPage page = new ContentPage().LoadFromXaml(xaml);
+
+                var labelWelcome = page.FindByName<Label>("lblWelcome");
+                labelWelcome.Text = "Texto alterado com sucesso.!";
+
+                await Prism.PrismApplicationBase.Current.MainPage.Navigation.PushAsync(page);
+            });
+
+            OpenNewPage2Command = new DelegateCommand(async () =>
+            {
+                string xaml = $"{RuntimePage.Header}" + 
+                              $"{RuntimePage.StartLabel}lblName1{RuntimePage.EndLabel}" +
+                              $"{RuntimePage.StartLabel}lblName2{RuntimePage.EndLabel}" +
+                              $"{RuntimePage.Footer}";
+
+
+                ContentPage page = new ContentPage().LoadFromXaml(xaml);
+
+                var label1 = page.FindByName<Label>("lblName1");
+                label1.FontSize = 20;
+                label1.Text = "Texto 1° alterado com sucesso.!";
+
+                var label2 = page.FindByName<Label>("lblName2");
+                label2.FontSize = 20;
+                label2.Text = "Texto 2° alterado com sucesso.!";
+
+
+                await Prism.PrismApplicationBase.Current.MainPage.Navigation.PushAsync(page);
+            });
+        }
+    }
+}
